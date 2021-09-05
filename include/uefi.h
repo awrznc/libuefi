b38f4cef2e79a2eab2992e0815ee58c01e52df6e
awrznc <awrznc@gmail.com>
2021-09-05 13:20:12 +0900

    Add EFI System Table.

--- include/uefi.h
+++ include/uefi.h
@@ -92,18 +92,6 @@ typedef union {
 #define EFI_MEMORY_CPU_CRYPTO 0x0000000000080000
 #define EFI_MEMORY_RUNTIME 0x8000000000000000
 
-
-/**
-  * EFI System Table
-  **/
-typedef struct {
-    UINT64 Signature;
-    UINT32 Revision;
-    UINT32 HeaderSize;
-    UINT32 CRC32;
-    UINT32 Reserved;
-} EFI_TABLE_HEADER;
-
 typedef struct {
     UINT16 ScanCode;
     CHAR16 UnicodeChar;
@@ -303,23 +291,6 @@ typedef struct {
     UINT8 Resp[];
 } EFI_CAPSULE_RESULT_VARIABLE_JSON;
 
-typedef struct {
-    EFI_TABLE_HEADER Hdr;
-    EFI_GET_TIME GetTime;
-    EFI_SET_TIME SetTime;
-    EFI_GET_WAKEUP_TIME GetWakeupTime;
-    EFI_SET_WAKEUP_TIME SetWakeupTime;
-    EFI_SET_VIRTUAL_ADDRESS_MAP SetVirtualAddressMap;
-    EFI_CONVERT_POINTER ConvertPointer;
-    EFI_GET_VARIABLE GetVariable;
-    EFI_GET_NEXT_VARIABLE_NAME GetNextVariableName;
-    EFI_SET_VARIABLE SetVariable;
-    EFI_GET_NEXT_HIGH_MONO_COUNT GetNextHighMonotonicCount;
-    EFI_RESET_SYSTEM ResetSystem;
-    EFI_UPDATE_CAPSULE UpdateCapsule;
-    EFI_QUERY_CAPSULE_CAPABILITIES QueryCapsuleCapabilities;
-    EFI_QUERY_VARIABLE_INFO QueryVariableInfo;
-} EFI_RUNTIME_SERVICES;
 
 typedef EFI_TPL (EFIAPI *EFI_RAISE_TPL) ( IN EFI_TPL NewTpl );
 typedef VOID (EFIAPI *EFI_RESTORE_TPL) ( IN EFI_TPL OldTpl );
@@ -432,6 +403,56 @@ typedef VOID (EFIAPI *EFI_COPY_MEM) ( IN VOID *Destination, IN VOID *Source, IN 
 typedef VOID (EFIAPI *EFI_SET_MEM) ( IN VOID *Buffer, IN UINTN Size, IN UINT8 Value );
 typedef EFI_STATUS (EFIAPI *EFI_CREATE_EVENT_EX) ( IN UINT32 Type, IN EFI_TPL NotifyTpl,  IN EFI_EVENT_NOTIFY NotifyFunction OPTIONAL, IN CONST VOID *NotifyContext OPTIONAL, IN CONST EFI_GUID *EventGroup OPTIONAL, OUT EFI_EVENT *Event );
 
+
+/**
+  * EFI System Table
+  **/
+typedef struct _EFI_TABLE_HEADER {
+    UINT64 Signature;
+    UINT32 Revision;
+    UINT32 HeaderSize;
+    UINT32 CRC32;
+    UINT32 Reserved;
+} EFI_TABLE_HEADER;
+
+#define EFI_SYSTEM_TABLE_SIGNATURE 0x5453595320494249
+#define EFI_2_90_SYSTEM_TABLE_REVISION ((2<<16) | (90))
+#define EFI_2_80_SYSTEM_TABLE_REVISION ((2<<16) | (80))
+#define EFI_2_70_SYSTEM_TABLE_REVISION ((2<<16) | (70))
+#define EFI_2_60_SYSTEM_TABLE_REVISION ((2<<16) | (60))
+#define EFI_2_50_SYSTEM_TABLE_REVISION ((2<<16) | (50))
+#define EFI_2_40_SYSTEM_TABLE_REVISION ((2<<16) | (40))
+#define EFI_2_31_SYSTEM_TABLE_REVISION ((2<<16) | (31))
+#define EFI_2_30_SYSTEM_TABLE_REVISION ((2<<16) | (30))
+#define EFI_2_20_SYSTEM_TABLE_REVISION ((2<<16) | (20))
+#define EFI_2_10_SYSTEM_TABLE_REVISION ((2<<16) | (10))
+#define EFI_2_00_SYSTEM_TABLE_REVISION ((2<<16) | (00))
+#define EFI_1_10_SYSTEM_TABLE_REVISION ((1<<16) | (10))
+#define EFI_1_02_SYSTEM_TABLE_REVISION ((1<<16) | (02))
+#define EFI_SPECIFICATION_VERSION EFI_SYSTEM_TABLE_REVISION
+#define EFI_SYSTEM_TABLE_REVISION EFI_2_90_SYSTEM_TABLE_REVISION
+
+typedef struct {
+    EFI_TABLE_HEADER Hdr;
+    EFI_GET_TIME GetTime;
+    EFI_SET_TIME SetTime;
+    EFI_GET_WAKEUP_TIME GetWakeupTime;
+    EFI_SET_WAKEUP_TIME SetWakeupTime;
+    EFI_SET_VIRTUAL_ADDRESS_MAP SetVirtualAddressMap;
+    EFI_CONVERT_POINTER ConvertPointer;
+    EFI_GET_VARIABLE GetVariable;
+    EFI_GET_NEXT_VARIABLE_NAME GetNextVariableName;
+    EFI_SET_VARIABLE SetVariable;
+    EFI_GET_NEXT_HIGH_MONO_COUNT GetNextHighMonotonicCount;
+    EFI_RESET_SYSTEM ResetSystem;
+    EFI_UPDATE_CAPSULE UpdateCapsule;
+    EFI_QUERY_CAPSULE_CAPABILITIES QueryCapsuleCapabilities;
+    EFI_QUERY_VARIABLE_INFO QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
+#define EFI_BOOT_SERVICES_SIGNATURE 0x56524553544f4f42
+#define EFI_BOOT_SERVICES_REVISION EFI_SPECIFICATION_VERSION
+
 typedef struct {
     EFI_TABLE_HEADER Hdr;
     EFI_RAISE_TPL RaiseTPL;
@@ -479,7 +500,7 @@ typedef struct {
     EFI_CREATE_EVENT_EX CreateEventEx;
 } EFI_BOOT_SERVICES;
 
-typedef struct{
+typedef struct {
     EFI_GUID VendorGuid;
     VOID *VendorTable;
 } EFI_CONFIGURATION_TABLE;
@@ -499,6 +520,54 @@ typedef struct {
     UINTN NumberOfTableEntries;
     EFI_CONFIGURATION_TABLE *ConfigurationTable;
 } EFI_SYSTEM_TABLE;
+
+#define ACPI_TABLE_GUID { 0xeb9d2d30, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define SAL_SYSTEM_TABLE_GUID { 0xeb9d2d32, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define SMBIOS_TABLE_GUID { 0xeb9d2d31, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define SMBIOS3_TABLE_GUID { 0xf2fd1544,  0x9794,  0x4a2c, { 0x99, 0x2e, 0xe5, 0xbb, 0xcf, 0x20, 0xe3, 0x94 } }
+#define MPS_TABLE_GUID { 0xeb9d2d2f, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define EFI_ACPI_TABLE_GUID { 0x8868e871, 0xe4f1, 0x11d3, { 0xbc, 0x22, 0x00, 0x80, 0xc7, 0x3c, 0x88, 0x81 } }
+#define EFI_ACPI_20_TABLE_GUID EFI_ACPI_TABLE_GUID
+#define ACPI_TABLE_GUID { 0xeb9d2d30, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } }
+#define ACPI_10_TABLE_GUID ACPI_TABLE_GUID
+#define EFI_JSON_CONFIG_DATA_TABLE_GUID { 0x87367f87, 0x1119, 0x41ce, { 0xaa, 0xec, 0x8b, 0xe0, 0x11, 0x1f, 0x55, 0x8a } }
+#define EFI_JSON_CAPSULE_DATA_TABLE_GUID { 0x35e7a725, 0x8dd2, 0x4cac, { 0x80, 0x11, 0x33, 0xcd, 0xa8, 0x10, 0x90, 0x56 } }
+#define EFI_JSON_CAPSULE_RESULT_TABLE_GUID { 0xdbc461c3, 0xb3de, 0x422a, { 0xb9, 0xb4, 0x98, 0x86, 0xfd, 0x49, 0xa1, 0xe5 } }
+
+#define EFI_RT_PROPERTIES_TABLE_GUID { 0xeb66918a, 0x7eef, 0x402a, { 0x84, 0x2e, 0x93, 0x1d, 0x21, 0xc3, 0x8a, 0xe9 } }
+
+typedef struct {
+    UINT16 Version;
+    UINT16 Length;
+    UINT32 RuntimeServicesSupported;
+} EFI_RT_PROPERTIES_TABLE;
+
+#define EFI_RT_PROPERTIES_TABLE_VERSION 0x1
+
+#define EFI_RT_SUPPORTED_GET_TIME 0x0001
+#define EFI_RT_SUPPORTED_SET_TIME 0x0002
+#define EFI_RT_SUPPORTED_GET_WAKEUP_TIME 0x0004
+#define EFI_RT_SUPPORTED_SET_WAKEUP_TIME 0x0008
+#define EFI_RT_SUPPORTED_GET_VARIABLE 0x0010
+#define EFI_RT_SUPPORTED_GET_NEXT_VARIABLE_NAME 0x0020
+#define EFI_RT_SUPPORTED_SET_VARIABLE 0x0040
+#define EFI_RT_SUPPORTED_SET_VIRTUAL_ADDRESS_MAP 0x0080
+#define EFI_RT_SUPPORTED_CONVERT_POINTER 0x0100
+#define EFI_RT_SUPPORTED_GET_NEXT_HIGH_MONOTONIC_COUNT 0x0200
+#define EFI_RT_SUPPORTED_RESET_SYSTEM 0x0400
+#define EFI_RT_SUPPORTED_UPDATE_CAPSULE 0x0800
+#define EFI_RT_SUPPORTED_QUERY_CAPSULE_CAPABILITIES 0x1000
+#define EFI_RT_SUPPORTED_QUERY_VARIABLE_INFO 0x2000
+
+#define EFI_MEMORY_ATTRIBUTES_TABLE_GUID { 0xdcfa911d, 0x26eb, 0x469f, {0xa2, 0x20, 0x38, 0xb7, 0xdc, 0x46, 0x12, 0x20 } }
+
+typedef struct {
+    UINT32 Version ;
+    UINT32 NumberOfEntries ;
+    UINT32 DescriptorSize ;
+    UINT32 Reserved;
+    // EFI_MEMORY_DESCRIPTOR Entry [1];
+} EFI_MEMORY_ATTRIBUTES_TABLE;
 
 #define EFI_LOADED_IMAGE_PROTOCOL_GUID { 0x5b1b31a1, 0x9562, 0x11d2, { 0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b } }
 #define EFI_LOADED_IMAGE_PROTOCOL_REVISION 0x1000
