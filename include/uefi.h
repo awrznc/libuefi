d6f0565923f865e164a33a3626c3455fd7e2787e
awrznc <awrznc@gmail.com>
2021-09-05 15:58:41 +0900

    Add EFI Loaded Image section.

--- include/uefi.h
+++ include/uefi.h
@@ -1006,6 +1006,10 @@ typedef struct {
     // EFI_MEMORY_DESCRIPTOR Entry [1];
 } EFI_MEMORY_ATTRIBUTES_TABLE;
 
+
+/**
+  * EFI Loaded Image
+  **/
 #define EFI_LOADED_IMAGE_PROTOCOL_GUID { 0x5b1b31a1, 0x9562, 0x11d2, { 0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b } }
 #define EFI_LOADED_IMAGE_PROTOCOL_REVISION 0x1000
 
