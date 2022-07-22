43376e7db4c62510d0effde54db1d269d73a8fc2
awrznc <awrznc@gmail.com>
2022-07-23 01:00:37 +0900

    Refactored unit test.

--- test/src/test_helper.h
+++ test/src/test_helper.h
@@ -3,6 +3,28 @@
 
 #include <uefi.h>
 
+_Static_assert(sizeof(BOOLEAN) == 1, "Expecting 8 bit integers");
+_Static_assert(sizeof(INTN) == 4, "Expecting 32 bit integers");
+_Static_assert(sizeof(UINTN) == 4, "Expecting 32 bit integers");
+_Static_assert(sizeof(INT8) == 1, "Expecting 8 bit integers");
+_Static_assert(sizeof(UINT8) == 1, "Expecting 8 bit integers");
+_Static_assert(sizeof(INT16) == 2, "Expecting 16 bit integers");
+_Static_assert(sizeof(UINT16) == 2, "Expecting 16 bit integers");
+_Static_assert(sizeof(INT32) == 4, "Expecting 32 bit integers");
+_Static_assert(sizeof(UINT32) == 4, "Expecting 32 bit integers");
+_Static_assert(sizeof(INT64) == 8, "Expecting 64 bit integers");
+_Static_assert(sizeof(UINT64) == 8, "Expecting 64 bit integers");
+_Static_assert(sizeof(INT128) == 16, "Expecting 128 bit integers");
+_Static_assert(sizeof(UINT128) == 16, "Expecting 128 bit integers");
+_Static_assert(sizeof(CHAR8) == 1, "Expecting 8 bit integers");
+_Static_assert(sizeof(CHAR16) == 2, "Expecting 16 bit integers");
+_Static_assert(sizeof(EFI_GUID) == 16, "Expecting 128 bit integers");
+
+_Static_assert(sizeof(EFI_MAC_ADDRESS) == 32, "Expecting 32 byte integers");
+_Static_assert(sizeof(EFI_IPv4_ADDRESS) == 4, "Expecting 4 byte integers");
+_Static_assert(sizeof(EFI_IPv6_ADDRESS) == 16, "Expecting 16 byte integers");
+_Static_assert(sizeof(EFI_IP_ADDRESS) == 16, "Expecting 16 byte integers");
+
 #define NULL 0x0
 
 #endif
---
+++ test/src/unit/miscellaneous_runtime_services/efi_main.c
@@ -0,0 +1,59 @@
+#include <test_helper.h>
+
+#define CAPSULE_HEADER_SIZE 1
+
+VOID reset_process() {}
+
+EFI_STATUS efi_main(
+    EFI_HANDLE ImageHandle,
+    EFI_SYSTEM_TABLE *SystemTable
+) {
+
+    EFI_STATUS status = EFI_SUCCESS;
+    if ( SystemTable == NULL || ImageHandle == NULL ) {
+        status = EFI_LOAD_ERROR;
+        return status;
+    }
+
+    // GetNextHighMonotonicCount() test
+    {
+        UINT32 count = 0;
+        status = SystemTable->RuntimeServices->GetNextHighMonotonicCount(&count);
+        if ( status != EFI_SUCCESS ) {
+            SystemTable->ConOut->OutputString( SystemTable->ConOut, L"GetNextHighMonotonicCount() test failed.\r\n");
+            SystemTable->RuntimeServices->ResetSystem( EfiResetShutdown, status, 0, NULL );
+        }
+    }
+
+    // UpdateCapsule() test
+    {
+        EFI_CAPSULE_HEADER* capsule_headers[CAPSULE_HEADER_SIZE] = { 0 };
+        EFI_CAPSULE_HEADER capsule_header = {
+            .CapsuleGuid = (EFI_GUID)EFI_MEMORY_RANGE_CAPSULE_GUID,
+            .HeaderSize = sizeof(EFI_CAPSULE_HEADER),
+            .Flags = (UINT32)CAPSULE_FLAGS_PERSIST_ACROSS_RESET,
+            .CapsuleImageSize = sizeof(EFI_CAPSULE_HEADER)
+        };
+
+        capsule_headers[0] = (EFI_CAPSULE_HEADER *)(&capsule_header);
+
+        status = SystemTable->RuntimeServices->UpdateCapsule(
+            capsule_headers,
+            ( UINTN )CAPSULE_HEADER_SIZE,
+            ( EFI_PHYSICAL_ADDRESS )( &reset_process )
+        );
+
+        if ( !(status == EFI_SUCCESS || status == EFI_UNSUPPORTED) ) {
+            SystemTable->ConOut->OutputString( SystemTable->ConOut, L"UpdateCapsule() test failed.\r\n");
+            SystemTable->RuntimeServices->ResetSystem( EfiResetShutdown, status, 0, NULL );
+        }
+    }
+
+    // ResetSystem() test
+    {
+        SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, 10, 0, NULL);
+        SystemTable->ConOut->OutputString( SystemTable->ConOut, L"ResetSystem() test failed.\r\n");
+    }
+
+    return EFI_SUCCESS;
+}
